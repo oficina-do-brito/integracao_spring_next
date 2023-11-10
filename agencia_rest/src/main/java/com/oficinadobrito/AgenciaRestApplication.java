e9fe80d11d844bf50f458f1497abc1c424482ab4
oficina-do-brito <clodoaldo.brtp4@gmail.com>
2023-11-09 23:01:44 -0300

    iniciado projeto

---
+++ agencia_rest/src/main/java/com/oficinadobrito/AgenciaRestApplication.java
@@ -0,0 +1,13 @@
+package com.oficinadobrito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenciaRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenciaRestApplication.class, args);
+	}
+
+}
